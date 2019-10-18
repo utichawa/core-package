ffa1429396fda8a942c7d6e2e455f4f6df708f37
Yassine Mechiche <mechiche.yassine@gmail.com>
2019-10-18 14:55:56 +0100

    adding config file

--- tests/LaravelTest.php
+++ tests/LaravelTest.php
@@ -48,6 +48,8 @@ class LaravelTest extends TestCase
             ->andReturn('some joke');
 
         $this->get('/chuck-norris')
+            ->assertViewIs('chuck-norris::joke')
+            ->assertViewHas('joke', 'some joke')
             ->assertStatus(200);
     }
 }
